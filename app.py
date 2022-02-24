eab631112e08eb62f01ecb4b138f0bd5e2f8fdf9
Bett Lameck <lameckbett02@gmail.com>
2022-02-24 14:47:51 +0300

    trial one

---
+++ app.py
@@ -0,0 +1,14 @@
+there 👋
+<a href="https://twitter.com/PremChapagains" class="button big">Twitter</a>
+<a href="https://www.linkedin.com/in/premchapagain/" class="button big">Linkedin</a>
+
+
+# 🧍 Prem chapagain
+####         🎓 Computer Science student
+#### 🎓 Diploma in computer
+
+#  💻 Expertise
+#### ✔️ Python Developer
+#### ✔️ Cyber security
+#### ✔️ Data science
+#### ✔️ machine learning
